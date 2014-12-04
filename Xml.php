c829288508cb642ecc65ad399ad685c159ce2596
Thomas Siebenmorgen <tsiebenmorgen@webomotion.de>
2014-12-04 10:40:32 +0100

    some function bodies added

--- Xml.php
+++ Xml.php
@@ -59,9 +59,9 @@ class Xml {
    */
   private $_NS_Fn       = null;
 
-  const ERR_CONTEXT_NOT_VALID = 'Kein gültiger Kontext!';
-  const ERR_FILE_NOT_EXIST    = 'Die Datei existiert nicht!';
-  const ERR_INVALID_PARAM     = 'Ungültiger Parameter: %s';
+  const ERR_CONTEXT_NOT_VALID = 'Xml::$context: Kein gültiger Kontext!';
+  const ERR_FILE_NOT_EXIST    = 'Xml::__construct: Parameter #1 ist kein gültiger Dateipfad!';
+  const ERR_INVALID_PARAM     = 'Xml::__construct: ungültiger Typ für Parameter #1';
   const ERR_INVALID_NS_FN     = 'Ungültige Funktion als NamespaceHandler angegeben!';
   const ERR_ITEM_NOT_ATTR     = 'Das Element ist kein Attribut!';
   const ERR_ITEM_NOT_TEXT     = 'Das Element ist kein Text!';
@@ -74,24 +74,61 @@ class Xml {
    * DOMDocument, DOMElement, ein Pfad zu einer XML-Datei oder ein
    * XML-String, auf den sich die Fassade bezieht.
    */
-  public function __construct($p = null) {}
+  public function __construct($p = null) {
+    $this->_saveDOMDocument($p);
+
+    $this->_DOMXPath = new DOMXPath($this->_DOMDocument);
+  }
 
   /**
    * @ignore
    */
-  public function __get($name) {}
+  public function __get($name) {
+    switch ($name) {
+      case 'context':
+        return $this->_ContextNode;
+        break;
+      case 'doc':
+        return $this->_DOMDocument;
+        break;
+      case 'formatOutput':
+        return $this->_DOMDocument->formatOutput;
+        break;
+      case 'preserveWhiteSpace':
+        return $this->_DOMDocument->preserveWhiteSpace;
+        break;
+      default:
+        return null;
+    }
+  }
 
   /**
    * @ignore
    */
-  public function __set($name, $value) {}
+  public function __set($name, $value) {
+    switch ($name) {
+      case 'formatOutput':
+        $this->_DOMDocument->formatOutput = (bool) $value;
+        break;
+      case 'preserveWhiteSpace':
+        $this->_DOMDocument->preserveWhiteSpace = (bool) $value;
+        break;
+      case 'context':
+        $this->_setContextNode($value);
+        break;
+      default:
+        return null;
+    }
+  }
 
   /**
    * Gibt den XML-Code hinter der Fassade zurück.
    *
    * @return string   XML-Code hinter der Fassade.
    */
-  public function __toString() {}
+  public function __toString() {
+    return $this->_DOMDocument->saveXML();
+  }
 
   /**
    * Gibt die Ergebnisse der übergebenen XPath-Query zurück.
@@ -134,7 +171,14 @@ class Xml {
    * @param   string        $strFile    Pfad zu einer XML-Datei.
    * @return  DOMDocument               DOMDocument aus der XML-Datei.
    */
-  private function _createDOMDocumentFromFile($strFile) {}
+  private function _createDOMDocumentFromFile($strFile) {
+    if (!is_file((string) $strFile)) {
+      throw new Exception(self::ERR_FILE_NOT_EXIST);
+    }
+    $DOMDocument = new DOMDocument('1.0', 'UTF-8');
+    $DOMDocument->load($strFile);
+    return $DOMDocument;
+  }
 
   /**
    * Diese default Funktion zum Umgang mit Namespaces entfernt die
@@ -194,7 +238,23 @@ class Xml {
    * XML-Code auf den sich die Fassade beziehen soll.
    * @return  void
    */
-  private function _saveDOMDocument($p) {}
+  private function _saveDOMDocument($p) {
+    if (is_string($p) && preg_match('/</', trim($p))) {
+      $this->_DOMDocument = new DOMDocument('1.0', 'UTF-8');
+      $this->_DOMDocument->loadXML($p);
+    } elseif (is_string($p)) {
+      $this->_DOMDocument = $this->_createDOMDocumentFromFile($p);
+    } elseif ($p instanceof DOMElement) {
+      $this->_DOMDocument = $p->ownerDocument;
+      $this->_ContextNode = $p;
+    } elseif ($p instanceof DOMDocument) {
+      $this->_DOMDocument = $p;
+    } elseif (is_null($p)) {
+      $this->_DOMDocument = new DOMDocument('1.0', 'UTF-8');
+    } else {
+      throw new Exception(self::ERR_INVALID_PARAM);
+    }
+  }
 
   /**
    * Speichert den DOMNode, auf den sich relative XPath beziehen sollen.
@@ -202,5 +262,13 @@ class Xml {
    * @param   null|false|XPath|DOMNode  $ContextNode
    * @return  void
    */
-  private function _setContextNode($ContextNode) {}
+  private function _setContextNode($ContextNode) {
+    if (is_null($ContextNode) || $ContextNode === false) {
+      $this->_ContextNode = null;
+    } elseif ($ContextNode instanceof DOMNode) {
+      $this->_ContextNode = $ContextNode;
+    } else {
+      throw new Exception(self::ERR_CONTEXT_NOT_VALID);
+    }
+  }
 }
