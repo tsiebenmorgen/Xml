e20ad88736af947616e59a99dd1a9ca8458738ea
Thomas Siebenmorgen <tsiebenmorgen@webomotion.de>
2014-12-05 01:54:41 +0100

    filecontent added

---
+++ XPath.php
@@ -0,0 +1,56 @@
+<?php namespace de\webomotion;
+
+use \DOMAttr;
+use \DOMDocument;
+use \DOMElement;
+use \DOMNameSpaceNode;
+use \DOMNode;
+use \DOMText;
+use \DOMXPath;
+use \Exception;
+
+class XPath {
+  const QUERY_IS_ATTRIBUTE = '/^attribute\:\:([a-zA-Z]+)$/';
+  const QUERY_IS_TEXT      = '/^text\(\)$/';
+  const QUERY_IS_RELATIVE  = '/^\./';
+  const GET_DIRNAME        = '/^(.+)(\/)?([^\/]+)$/U';
+  const GET_BASENAME       = '/^(?:.+)(?:\/)?([^\/]+)$/U';
+
+  private $_strQuery = '';
+  private $_xml = null;
+
+  public function __construct(Xml $xml, $str) {
+    $this->_xml = $xml;
+    $this->_strQuery = (string) $str;
+  }
+
+  public function __get($name) {
+    switch ($name) {
+      case 'basename':
+        return preg_replace(self::GET_BASENAME, '$1', $this->_strQuery);
+      case 'dirname':
+        return preg_replace(self::GET_DIRNAME, '$1', $this->_strQuery);
+      case 'isAbsolute':
+        return !$this->isRelative;
+      case 'isAttribute':
+        return (bool) preg_match(self::QUERY_IS_ATTRIBUTE, $this->basename);
+      case 'isText':
+        return (bool) preg_match(self::QUERY_IS_TEXT, $this->basename);
+      case 'isRelative':
+        return (bool) preg_match(self::QUERY_IS_RELATIVE, $this->dirname);
+    }
+  }
+
+  public function __toString() {
+    return (string) $this->_strQuery;
+  }
+
+  public function execute() {
+    $DOMXPath = new DOMXPath($this->_xml->doc);
+    if ($this->isRelative) {
+      return $DOMXPath->query((string) $this, $this->_xml->context);
+    } else {
+      return $DOMXPath->query((string) $this, $this->_xml->doc->documentElement);
+    }
+  }
+}
