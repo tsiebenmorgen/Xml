ff0a8b93e56b2c1ada2bb662c7eb41f1ce1990b9
Thomas Siebenmorgen <tsiebenmorgen@webomotion.de>
2014-12-05 02:09:42 +0100

    some tests added, missing test for execute()

---
+++ Tests/XPath.Test.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once dirname(__FILE__) . '/../XPath.php';
+
+use de\webomotion\XPath;
+
+/**
+ * Tests für die XPath-Klasse
+ *
+ * @author Thomas Siebenmorgen <tsiebenmorgen@webomotion.de>
+ */
+class XPathTest extends PHPUnit_Framework_TestCase {
+  private $absoluteAttributeQuery = '/kml/attribute::xmlns';
+  private $relativeTextQuery      = './text()';
+
+  public function testToString() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($this->absoluteAttributeQuery, (string) $xpath);
+  }
+
+  public function testGetIsAttribute() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->isAttribute, true);
+  }
+
+  public function testGetBasename() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->basename, 'attribute::xmlns');
+  }
+
+  public function testGetBasename2() {
+    $xpath = new XPath($this->relativeTextQuery);
+
+    $this->assertEquals($xpath->basename, 'text()');
+  }
+
+  public function testGetDirname() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->dirname, '/kml');
+  }
+
+  public function testGetDirname2() {
+    $xpath = new XPath($this->relativeTextQuery);
+
+    $this->assertEquals($xpath->dirname, '.');
+  }
+
+  public function testGetIsText() {
+    $xpath = new XPath($this->relativeTextQuery);
+
+    $this->assertEquals($xpath->isText, true);
+  }
+
+  public function testGetIsAbsoluteFalse() {
+    $xpath = new XPath($this->relativeTextQuery);
+
+    $this->assertEquals($xpath->isAbsolute, false);
+  }
+
+  public function testGetIsRelative() {
+    $xpath = new XPath($this->relativeTextQuery);
+
+    $this->assertEquals($xpath->isRelative, true);
+  }
+
+  public function testGetIsAbsolute() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->isAbsolute, true);
+  }
+
+  public function testGetIsRelativeFalse() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->isRelative, false);
+  }
+
+  public function testGetIsTextFalse() {
+    $xpath = new XPath($this->absoluteAttributeQuery);
+
+    $this->assertEquals($xpath->isText, false);
+  }
+}
